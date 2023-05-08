02e69c1b2df42f88775e3874242d7404902e5885
tf1993614 <61369770+tf1993614@users.noreply.github.com>
2023-05-08 14:00:00 +0930

    Add files via upload

---
+++ PreQC and pesudoalignment pipeline/Pipeline/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+ml Python/3.6.1-foss-2016b
+
+# 1st para is input path
+# 2nd para is template
+# 3rd para is output path
+input="/hpcfs/users/a1763076/pig_oocyte/fastq_files/"
+output="/hpcfs/users/a1763076/pig_oocyte/json_output"
+template="/hpcfs/users/a1763076/pig_oocyte/json_input/template.json"
+index="/hpcfs/users/a1763076/genome_index/Kallisto/pig/pig_kallisto_ensembl_index"
+
+echo "start to gerenate input json files."
+python getInput.py -inputPath ${input} -templatejson ${template} -outputPath ${output} -mappingIndex ${index}
+echo "finish generating json files."
+
+for i in ${output}/*.json; do sbatch slurm_RNAseqWDL.sh ${i}; done
